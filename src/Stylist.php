d2557a21393cbb698fb107e6db1c56fa51183ee1
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-08-21 10:04:38 -0700

    add getName to Stylist method: pass

--- src/Stylist.php
+++ src/Stylist.php
@@ -3,12 +3,22 @@
     {
         private $name;
         private $id;
-    }
 
-    function __construct($name, $id = null)
-    {
-        $this->name = $name;
-        $this->id = $id;
-    }
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
 
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+    }
 ?>
